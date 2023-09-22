060850989bc7fcb3387b92b5e645487c2eb78054
Vidas <money4@inbox.lt>
2023-09-22 13:22:33 +0200

    Day 1-6

--- service/services/models.py
+++ service/services/models.py
@@ -1,11 +1,21 @@
 from django.core.validators import MaxValueValidator
 from django.db import models
 from clients.models import Client
+from services.tasks import set_price
+
 
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=100)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+    def save(self, *args, **kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(self.id)
 
 
 class Plan(models.Model):
@@ -15,17 +25,27 @@ class Plan(models.Model):
         ('discount', 'Discount'),
     )
 
-
     plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
-    discount_percent =models.PositiveIntegerField(default=0, validators=[
+    discount_percent = models.PositiveIntegerField(default=0, validators=[
         MaxValueValidator(100)
     ])
 
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+    def save(self, *args, **kwargs):
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(self.id)
+
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return f"Plan type: {self.plan_type}"
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
-
+    price = models.PositiveIntegerField(default=0)
