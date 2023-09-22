c875d58aa9406260d73656334f78c65219db6a27
Vidas <money4@inbox.lt>
2023-09-22 12:07:39 +0200

    Day Aggregate repair

--- service/services/views.py
+++ service/services/views.py
@@ -1,5 +1,4 @@
 from django.db.models import Prefetch, F, Sum
-from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -8,22 +7,21 @@ from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    # queryset = Subscription.objects.all().prefetch_related('client__user')
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
-
-        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
-
+        Prefetch('client',
+     queryset=Client.objects.all().select_related('user').only('company_name',
+                                                               'user__email'))
+        ).annotate(price=F('service__full_price') -
+                         F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
-
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
 
-
         response_data = {'result': response.data}
         response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
-        response_data = response_data
+        response.data = response_data
+
         return response
